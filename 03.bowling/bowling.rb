3b343c4a233895bc3777f2b29d64545d7b298a37
halllllll <hjod1172@yahoo.co.jp>
2026-06-15 12:58:43 +0900

    create computing bowling score program

---
+++ 03.bowling/bowling.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+score = ARGV[0]
+scores = score.split(',').map { |n| n == 'X' ? 10 : n.to_i }
+
+# 合計点がわかればいいので、フレームごとに区切らず、自点を加算する方針
+scores_result = scores.clone
+frame_count = 0
+bowl_count = 0
+
+while frame_count < 9
+  if scores[bowl_count] == 10 # strike
+    scores_result[bowl_count + 1] += scores[bowl_count + 1]
+    scores_result[bowl_count + 2] += scores[bowl_count + 2]
+    bowl_count += 1
+  elsif scores[bowl_count] + scores[bowl_count + 1] == 10 # spare
+    scores_result[bowl_count + 2] += scores[bowl_count + 2]
+    bowl_count += 2
+  else
+    bowl_count += 2
+  end
+  frame_count += 1
+end
+
+# p "result: #{scores_result}"
+p scores_result.sum
