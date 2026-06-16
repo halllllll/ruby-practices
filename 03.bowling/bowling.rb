99a618ba1f50694eca9c67d6cbe1ffe2925863dd
halllllll <hjod1172@yahoo.co.jp>
2026-06-16 10:00:09 +0900

    合計点を配列を使わずに単純な足し算にした

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -5,17 +5,17 @@ score = ARGV[0]
 scores = score.split(',').map { |n| n == 'X' ? 10 : n.to_i }
 
 # 合計点がわかればいいので、フレームごとに区切らず、自点を加算する方針
-scores_result = scores.clone
+scores_result = scores.sum
 frame_count = 0
 bowl_count = 0
 
 while frame_count < 9
   if scores[bowl_count] == 10 # strike
-    scores_result[bowl_count + 1] += scores[bowl_count + 1]
-    scores_result[bowl_count + 2] += scores[bowl_count + 2]
+    scores_result += scores[bowl_count + 1]
+    scores_result += scores[bowl_count + 2]
     bowl_count += 1
   elsif scores[bowl_count] + scores[bowl_count + 1] == 10 # spare
-    scores_result[bowl_count + 2] += scores[bowl_count + 2]
+    scores_result += scores[bowl_count + 2]
     bowl_count += 2
   else
     bowl_count += 2
@@ -23,4 +23,4 @@ while frame_count < 9
   frame_count += 1
 end
 
-puts scores_result.sum
+puts scores_result
