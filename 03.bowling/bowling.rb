9eeb18f7eb6ed61aa6dc72ae534233b5e9836298
halllllll <hjod1172@yahoo.co.jp>
2026-06-21 18:42:57 +0900

    remove while loop

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -6,10 +6,9 @@ scores = score.split(',').map { |n| n == 'X' ? 10 : n.to_i }
 
 # 合計点がわかればいいので、フレームごとに区切らず、自点を加算する方針
 scores_result = scores.sum
-frame_count = 0
 bowl_count = 0
 
-while frame_count < 9
+9.times do
   if scores[bowl_count] == 10 # strike
     # 2投先まで加点
     scores_result += scores[bowl_count + 1, 2].sum
@@ -21,7 +20,6 @@ while frame_count < 9
   else
     bowl_count += 2
   end
-  frame_count += 1
 end
 
 puts scores_result
