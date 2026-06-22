e504b7c28dec5b0c1b858f28978f568d4c614812
halllllll <hjod1172@yahoo.co.jp>
2026-06-22 12:27:57 +0900

    calコマンドでの表示位置に寄せる

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -21,7 +21,7 @@ if params[:month] || params[:year]
 end
 
 # ヘッダーの出力
-puts "#{target_calendar.mon}月 #{target_calendar.year}".rjust(13)
+puts "#{target_calendar.mon}月 #{target_calendar.year}".rjust(14)
 puts WEEKDAY_HEADER.join(" ")
 
 # 最初の日と最後の日を取得する
